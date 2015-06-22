f36b3c771778de4f6431888e6ff6006377d5a580
Jared Parsons <jaredpar@microsoft.com>
2015-06-22 16:08:19 -0400

    Simple type provider created
    Can provide a type with no real information in it.  Just a type.  But
    progress which is fun.

---
+++ HelloWorld/HelloWorld.fs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+
+open Samples.FSharp.ProvidedTypes
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
+
+[<TypeProvider>]
+type HelloWorld() as this = 
+    inherit TypeProviderForNamespaces()
+
+    do
+        let assembly = Assembly.GetExecutingAssembly()
+        let ns = "TypeProvider.Samples"
+        let t = ProvidedTypeDefinition(assembly, ns, "Container", None)
+        let all = [t]
+        this.AddNamespace(ns, all)
+
